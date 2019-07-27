ed004cedad62f30c9b212eeca8667fa2e0d873d5
Raul Sposito <raulsposito@gmail.com>
2019-07-27 13:51:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,9 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  your_hash = {
+    "my car" => suzuki
+    "my bike" => verde
+    "my guitar" => schecter
+  }
 end
 
 
